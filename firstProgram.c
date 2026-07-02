109b8080728923c3953cc8f6db0a67ea15204ac9
Victor Guimarães <victor.g.b.guimaraes@gmail.com>
2026-07-02 14:09:12 +0100

    First Commit

---
+++ firstProgram.c
@@ -0,0 +1,14 @@
+#include <stdio.h> //stdio.h means standard input and output library
+
+//Must always include a int main(){} for program to work
+int main(){
+    printf("Hello World!\n"); //printf("") is used for printing text in terminal
+    printf("A small step for my career, and a big step for today!");
+    return 0;
+}
+
+/*Because of the int main function the code expects something in return,
+* so the command return 0 is used for sending back the integer value of 0.
+* This means that the code has successfuly ran. If it returns 1, then it
+* means that a problem has occurred.
+*/
