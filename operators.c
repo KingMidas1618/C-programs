a16c0f0d91bddb228f22e0752c021bb801a21f14
Victor Guimarães <victor.g.b.guimaraes@gmail.com>
2026-07-14 15:23:03 +0100

    Operators

---
+++ operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    //z = x+y; addition
+    //z = x-y; subtraction
+    //z = x*y; multiplication
+    //z = x/y; division (careful when doing a integer division as it doesn't store decimals)
+    //z = x%y; remainder of division
+    //z++; increment (adds one to the number)
+    //z--; decrement (subtracts one to the number)
+
+    //Augmented assigned operators
+    //x = x+2; <=> x+=2;
+    //x = x-2; <=> x-=2;
+    //x = x*2; <=> x*=2;
+    //x= x/2; <=> x/=2;
+
+    printf("%d\n", z);
+
+    return 0;
+}
