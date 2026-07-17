5ad9e433df3930d12eb3494e2913f0af8a22d0db
Victor Guimarães <victor.g.b.guimaraes@gmail.com>
2026-07-17 14:38:04 +0100

    Math Functions

---
+++ mathFunctions.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    float x = 10;
+
+    //x = sqrt(x);
+    //x = pow(x, 4);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x = abs(x);
+    //x = log(x);
+    //x = log10(x);
+    //x = sin(x);
+    //x = cos(x);
+    //x = tan(x);
+
+    printf("%f\n", x);
+
+    return 0;
+}
+
+/*
+sqrt():     Determines the square root of any number.
+
+pow(x, 4):  Increases the value of, in this case, x to the power
+            of 4. The first parameter is for the variable or
+            any number, and the second parameter is for the 
+            power in which the variable will be increased by.
+
+round():    Rounds number to the nearest integer.
+
+ceil():     Always rounds UP to the next integer.
+
+floor():    Always rounds DOWN to the next integer.
+
+abs():      Gives the absolute value of the number.
+
+log():      Returns the natural logarithm
+
+log10():    Returns the base 10 logarithm
+
+sin(x),
+cos(x),
+and tan(x): Returns the sine, cosine, or tangent of x 
+            (x is in radians).
+*/
